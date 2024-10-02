587cbbcefe0cbae98b8448244b02bc773ae1c653
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2024-10-01 20:24:43 -0400

    ok
    asdf

---
+++ ApiTarefas_Aula01/Controllers/TarefaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTarefas_Aula01.Controllers
+{
+    [ApiController]
+    [Route("tarefas")]
+
+    public class TarefaController : Controller
+    {
+        [HttpGet]
+        public IActionResult ListarTarefas()
+        {
+
+            return Ok(ObterTarefas());
+        }
+
+        private List<string> ObterTarefas() {
+            List<string> tarefas = new List<string>() { "ESTUDAR", "DORMIR", "COMER", "SONECA"};
+
+            tarefas.Add("Estudar - Metodos HTTPS");
+            return tarefas;
+        }
+    }
+}
